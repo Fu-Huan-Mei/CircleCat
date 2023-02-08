6e9b11de7b59d063fb8738d37d9de3ef546da22e
FuHuanMei <3533643117@qq.com>
2023-02-08 14:46:19 +0800

    提交今天学习内容

--- YuClass/practice.py
+++ YuClass/practice.py
@@ -1,3 +1,42 @@
+def print_something(num):
+	print(num)
+
+print_something(21)
+# def get_first_num():
+#  return 3
+
+# def get_second_num():
+#  return 4
+
+# def times_op():
+#  return get_first_num() * get_second_num()
+
+# print(times_op())
 # years = input()
 # print(years,"岁")
-print("my+name")
+# a = 10
+
+# def get_first_name():
+#  return "Circle"
+
+# def get_last_name():
+#  return "Cat"
+
+# print(get_first_name() + "," + get_last_name())# b = 3
+# print(a, b)
+# num_cats = int(input())
+# num_dogs = int(input())
+# sum = num_cats + num_dogs
+# print(sum)
+# print()
+# print("hello")
+# age = int(input())
+# print("my age is " , age)
+# x = int(input())
+# y = int(input())
+# x = x // y
+# print(x)
+# x = x // y
+# print(x)
+# x = x // y
+# print(x)
